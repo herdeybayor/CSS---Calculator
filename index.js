3c9c7f3c1d51a9f84940c83b00ce82594c472a93
herdeybayor <shereefdeen4real@gmail.com>
2021-08-11 13:46:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+$(".btn").click(function() {
+    var displayText = $(".display").text();
+    var newText = $(this).text();
+    $(".display").text(displayText + newText);
+})
+
+$(document).keypress
+
+
+$(".ac").click(() => {
+    var displayText = $(".display").text();
+    $(".display").text("");
+})
+
+$(".equal").click(()=>{
+    var result = $(".display").text();
+    console.log(result);
+})
