152771a16d2fd87e09c6db564dc85854ac1758f4
herdeybayor <shereefdeen4real@gmail.com>
2021-08-12 19:21:57 +0100

    times and keypress

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-$(".btn").click(function() {
+// Button Click Event
+$(".btn").click(function () {
     var displayText = $(".display").text();
     var newText = $(this).text();
     $(".display").text(displayText + newText);
 })
 
-$(document).keypress
+// KeyPress Event
+$(document).keypress(function (event) {
+    var a = true;
+    var b = isNaN(event.key);
+    if (event.key == "Enter") {
+        $(".display").text(eval($(".display").text()));
+    }
+    else if(event.key == "*"){
+        var displayText = $(".display").text();
+        $(".display").text(displayText + event.key.replace("*", "\u00d7"));
+    }
+    else if (event.key == "*" || event.key == "/" || event.key == "-" || event.key == "+") {
+        var displayText = $(".display").text();
+        $(".display").text(displayText + event.key);
+    } else if (a !== b) {
+        var displayText = $(".display").text();
+        $(".display").text(displayText + event.key);
+    } else {
+        console.log(event.key);
+    }
+})
 
-
+// AC click Button
 $(".ac").click(() => {
     var displayText = $(".display").text();
     $(".display").text("");
 })
 
-$(".equal").click(()=>{
-    var result = $(".display").text();
-    $(".display").text(eval($(".display").text()));
+// Equal Button
+$(".equal").click(() => {
+    var result = $(".display").text().replace("\u00d7", "*");
+    $(".display").text(eval(result));
 })
